872ccdb002e9ac66c1871b300a9243fab75de72e
ziad328 <ziad32seleem@gmail.com>
2024-07-11 22:48:26 +0300

    Initial Design for Product List + Fonts: poppins with all its types + Colors + Drawables

---
+++ app/src/main/java/com/example/androidtask/productList/activity/ProductListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.androidtask.productList.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.androidtask.R
+
+class ProductListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_list)
+    }
+}
